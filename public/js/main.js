e8f1d1c8bfccc8f5a1444881ff8236d3783e5649
Yassine Ebd <elbodmoussiyassine@gmail.com>
2022-12-21 11:40:25 +0100

    fin moitié

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+let tab =[];
+let i;
+i = prompt(`Entrez 10 chiffres`)
+
+for (let i = 0; i < 10; i++) {
+    
+    i = prompt(`Entrez 10 chiffres`)
+    tab.push(i);
+    console.log(tab);
+    console.log(tab.sort());
+    console.log(tab.reverse());
+}
+
